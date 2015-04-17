6c3fb91c2d8c947b874db850d4a689b4f3a0a7fb
swapniljoshi15 <swapnil.joshi1590@gmail.com>
2015-04-17 10:20:13 -0700

    Added Home page for user and admin

--- src/main/java/com/movieproject/controller/MovieAppController.java
+++ src/main/java/com/movieproject/controller/MovieAppController.java
@@ -3,17 +3,12 @@ package com.movieproject.controller;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.autoconfigure.EnableAutoConfiguration;
 import org.springframework.context.annotation.ImportResource;
-import org.springframework.stereotype.Component;
-import org.springframework.test.context.ContextConfiguration;
 import org.springframework.web.bind.annotation.PathVariable;
-import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.ResponseBody;
 import org.springframework.web.bind.annotation.RestController;
 import org.springframework.web.servlet.config.annotation.WebMvcConfigurerAdapter;
-
-import scala.annotation.meta.setter;
 
 import com.movieproject.dto.DemoDto;
 import com.movieproject.interfaces.DemoInterface;
