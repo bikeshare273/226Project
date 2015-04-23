0b0a5f3439cdd9689c91cda6131dc63e57e8adef
vireshjivane <viresh.jivane@sjsu.edu>
2015-04-23 12:18:29 -0700

    Included AddHistory API in Controller.

--- src/main/java/com/movieproject/controller/MovieAppController.java
+++ src/main/java/com/movieproject/controller/MovieAppController.java
@@ -49,6 +49,7 @@ import com.movieproject.entities.Users;
 import com.movieproject.implementation.AnalyticsImpl;
 import com.movieproject.implementation.CommentImpl;
 import com.movieproject.implementation.MovieImpl;
+import com.movieproject.implementation.UserHistoryImpl;
 import com.movieproject.implementation.UserImpl;
 import com.movieproject.implementation.UserRatingsImpl;
 import com.movieproject.interceptor.SessionValidatorInterceptor;
@@ -61,341 +62,350 @@ import com.movieproject.util.MovieAppUtil;
 @RequestMapping("/api/v1/*")
 @Import(MovieAppBeansConfiguration.class)
 public class MovieAppController extends WebMvcConfigurerAdapter {	
-	
+
 	@Autowired
 	DemoInterface demoInterface;
-	
+
 	@Autowired
 	ITestDao testDao;
-	
+
 	@Autowired
 	UserImpl userImpl;
-	
+
 	@Autowired
 	MovieImpl movieImpl;
-	
+
 	@Autowired
 	CommentImpl commentImpl;
-	
+
 	@Autowired
 	UserRatingsImpl userRatingImpl;
-	
+
 	@Autowired
 	IAuthInterfaceForLogin authInterfaceForLogin;
-	
+
 	@Autowired
 	SessionValidatorInterceptor sessionValidatorInterceptor;
-	
+
 	@Autowired
 	MovieAppUtil movieAppUtils;
-	
+
 	@Autowired
 	AnalyticsImpl analyticsImpl;
-	
-	
-/***********************************************************************************************/
-    
-							/* Login, Logout and Session Management */
 
-/***********************************************************************************************/
-	
-    @Override
-    public void addInterceptors(InterceptorRegistry registry) {
-        //registry.addInterceptor(sessionValidatorInterceptor).addPathPatterns("/api/v1/users/*");
-        registry.addInterceptor(sessionValidatorInterceptor).addPathPatterns("/api/v1/loggedin");
-    }
-	
-    @RequestMapping(value="/loggedin", method = RequestMethod.GET)
+	@Autowired
+	UserHistoryImpl userHistoryImpl;
+
+
+
+	/***********************************************************************************************/
+
+	/* Login, Logout and Session Management */
+
+	/***********************************************************************************************/
+
+	@Override
+	public void addInterceptors(InterceptorRegistry registry) {
+		//registry.addInterceptor(sessionValidatorInterceptor).addPathPatterns("/api/v1/users/*");
+		registry.addInterceptor(sessionValidatorInterceptor).addPathPatterns("/api/v1/loggedin");
+	}
+
+	@RequestMapping(value="/loggedin", method = RequestMethod.GET)
 	@ResponseBody
 	private boolean logeedin() {
-        return true;
+		return true;
 	}
-    
-    
-    @RequestMapping("/login")
+
+
+	@RequestMapping("/login")
 	@ResponseBody
 	private LoginDTO login(@Valid @RequestBody LoginDTO loginDTO, HttpServletResponse response) {
 		loginDTO = authInterfaceForLogin.login(loginDTO);
-        response.addCookie(new Cookie("sessionid", Integer.toString(loginDTO.getSessionId())));
-        response.addCookie(new Cookie("username", loginDTO.getUsername()));
-        response.addCookie(new Cookie("userid", Integer.toString(loginDTO.getUserid())));
-        return loginDTO;
+		response.addCookie(new Cookie("sessionid", Integer.toString(loginDTO.getSessionId())));
+		response.addCookie(new Cookie("username", loginDTO.getUsername()));
+		response.addCookie(new Cookie("userid", Integer.toString(loginDTO.getUserid())));
+		return loginDTO;
 	}
-	
+
 	@RequestMapping("/logout")
 	@ResponseBody
 	private boolean logout(HttpServletResponse response) {
-		
-        response.addCookie(new Cookie("sessionid", ""));
-        response.addCookie(new Cookie("username", ""));
-        response.addCookie(new Cookie("userid", ""));
-        
-        return true;
+
+		response.addCookie(new Cookie("sessionid", ""));
+		response.addCookie(new Cookie("username", ""));
+		response.addCookie(new Cookie("userid", ""));
+
+		return true;
 	}
-	
-	
+
+
 	@ResponseStatus(HttpStatus.OK)
 	@RequestMapping(value = "/verifyUniqueUsername/{username}", method = RequestMethod.GET)
 	@ResponseBody
 	public boolean checkUniqueUsername(@PathVariable String username) {
 		return userImpl.checkUniqueUsername(username);
 	}
-    	
-    
-/***********************************************************************************************/
-    
-									/* User Entity APIs */
-	
-/***********************************************************************************************/
-	
+
+
+	/***********************************************************************************************/
+
+	/* User Entity APIs */
+
+	/***********************************************************************************************/
+
 	@ResponseStatus(HttpStatus.CREATED)
 	@RequestMapping(value = "/users", method = RequestMethod.POST)
 	@ResponseBody
 	public UserDTO createUser(@Valid @RequestBody UserDTO user) {
 		return userImpl.createUser(user);
 	}
-	
-	
+
+
 	@ResponseStatus(HttpStatus.CREATED)
 	@RequestMapping(value = "/updateuser", method = RequestMethod.POST)
 	@ResponseBody
 	public UserDTO updateUser(@Valid @RequestBody UserDTO user, @CookieValue("userid") int userid) {
-		
+
 		user.setUserid(userid);
-		
+
 		return userImpl.updateUser(user);
 	}
-	
-	
+
+
 	@ResponseStatus(HttpStatus.OK)
 	@RequestMapping(value = "/users/{userid}", method = RequestMethod.GET)
 	@ResponseBody
 	public UserDTO getUser(@PathVariable Integer userid) {
-		
+
 		return userImpl.getUser(userid);		
 	}
-	
-	
+
+
 	@ResponseStatus(HttpStatus.OK)
 	@RequestMapping(value = "/fetchuser", method = RequestMethod.GET)
 	@ResponseBody
 	public UserDTO fetchUser(@CookieValue("userid") int userid) {
-		
+
 		return userImpl.getUser(userid);		
-	
+
 	}
-	
+
 	@ResponseStatus(HttpStatus.OK)
 	@RequestMapping(value = "/fetchallusers", method = RequestMethod.GET)
 	@ResponseBody
 	public List<Users> fetchAllUsers() {
-		
-		return userImpl.getAllUsers();		
-	
-	}
-	
-    
-/***********************************************************************************************/
-										
-									/* Movie Entity APIs */
-	
-/***********************************************************************************************/
 
-	
+		return userImpl.getAllUsers();		
+
+	}
+
+
+	/***********************************************************************************************/
+
+	/* Movie Entity APIs */
+
+	/***********************************************************************************************/
+
+
 	@ResponseStatus(HttpStatus.CREATED)
 	@RequestMapping(value = "/addmovie", method = RequestMethod.POST)
 	@ResponseBody
 	public void addMovie(@Valid @RequestBody MovieDTO movie) {
-		
+
 		movieImpl.addMovie(movie);
 	}
-		
+
 	@ResponseStatus(HttpStatus.OK)
 	@RequestMapping(value = "/getmoviebyid", method = RequestMethod.POST)
 	@ResponseBody
-	public Movie getMovie(@Valid @RequestBody SearchDTO searchDTO) {
-		
+	public Movie getMovie(@Valid @RequestBody SearchDTO searchDTO, @CookieValue("userid") int userid) {
+
 		Integer movieid = Integer.parseInt(searchDTO.getSearchString()); 
-			
-		return movieImpl.getMovie(movieid);
+
+		Movie movie =  movieImpl.getMovie(movieid);
+
+		if(movie == null) {return null;}
+		else{
+				userHistoryImpl.addUserHistoryEntry(userid, movieid);
+				return movie;
+		}
 	}
-	
-	
+
 	@ResponseStatus(HttpStatus.OK)
 	@RequestMapping(value = "/fetchallmovies", method = RequestMethod.GET)
 	@ResponseBody
 	public List<Movie> fetchAllMovies() {
-		
+
 		return movieImpl.getAllMovies();		
-	
+
 	}
-	
+
 	@ResponseStatus(HttpStatus.OK)
 	@RequestMapping(value = "/getmoviesbyname", method = RequestMethod.POST)
 	@ResponseBody
 	public List<Movie> getMoviesByName(@Valid @RequestBody SearchDTO searchDTO) {
-		
+
 		String moviename = searchDTO.getSearchString();
-		
+
 		return movieImpl.getMoviesByName(moviename);
-	
+
 	}	
-	
+
 	@ResponseStatus(HttpStatus.OK)
 	@RequestMapping(value = "/getmoviesofcategory", method = RequestMethod.POST)
 	@ResponseBody
 	public List<Movie> getMoviesByCategoty(@Valid @RequestBody SearchDTO searchDTO) {
-		
+
 		Integer categoryid = Integer.parseInt(searchDTO.getSearchString());
 		return movieImpl.getAllMoviesForCategory(categoryid);
-	
+
 	}
-	
+
 	@ResponseStatus(HttpStatus.OK)
 	@RequestMapping(value = "/getmoviesbyactorname", method = RequestMethod.POST)
 	@ResponseBody
 	public List<Movie> getMoviesByActorName(@Valid @RequestBody SearchDTO searchDTO) {
-		
+
 		String actorNameSearchString = searchDTO.getSearchString(); 
-		
+
 		return movieImpl.getMovieByActorName(actorNameSearchString);
-	
+
 	}
-	
+
 	@ResponseStatus(HttpStatus.OK)
 	@RequestMapping(value = "/getmoviesforlanguage", method = RequestMethod.POST)
 	@ResponseBody
 	public List<Movie> getMoviesForLanguage(@Valid @RequestBody SearchDTO searchDTO) {
-		
+
 		return movieImpl.getAllMoviesForLanguage(searchDTO.getSearchString());
-	
+
 	}
-	
+
 	@ResponseStatus(HttpStatus.OK)
 	@RequestMapping(value = "/getmoviesforyear", method = RequestMethod.POST)
 	@ResponseBody
 	public List<Movie> getMoviesForYear(@Valid @RequestBody SearchDTO searchDTO) {
-		
+
 		Integer year = Integer.parseInt(searchDTO.getSearchString());
-				
+
 		return movieImpl.getAllMoviesForYear(year);
-	
+
 	}
-			
+
 	@ResponseStatus(HttpStatus.OK)
 	@RequestMapping(value = "/deletemovie", method = RequestMethod.POST)
 	@ResponseBody
 	public void deleteMovie(@Valid @RequestBody SearchDTO searchDTO) {
-		
+
 		Integer movieid = Integer.parseInt(searchDTO.getSearchString());
 		movieImpl.deleteMovie(movieid);
-	
-	}
-	
-/***********************************************************************************************/
-	
-								/* Comment APIs */
 
-/***********************************************************************************************/
-		
+	}
+
+	/***********************************************************************************************/
+
+	/* Comment APIs */
+
+	/***********************************************************************************************/
+
 	@ResponseStatus(HttpStatus.CREATED)
 	@RequestMapping(value = "/addcomment", method = RequestMethod.POST)
 	@ResponseBody
 	public void addMovie(@Valid @RequestBody CommentDTO commentDTO, @CookieValue("userid") int userid) {
-    
+
 		commentImpl.addComment(commentDTO, userid);
-		
+
 	}
-	
-	
-    
+
+
+
 	@ResponseStatus(HttpStatus.OK)
 	@RequestMapping(value = "/getcommentsformovie", method = RequestMethod.POST)
 	@ResponseBody
 	public List<Comment> getAllCommentForAMovie(@Valid @RequestBody SearchDTO searchDTO) {
-		
-		Integer movieid = Integer.parseInt(searchDTO.getSearchString());
-    	
-		return commentImpl.getAllCommentsForMovie(movieid);
-		
-	}
-	
-/***********************************************************************************************/
-	
-									/* Rating APIs */
 
-/***********************************************************************************************/
-    
+		Integer movieid = Integer.parseInt(searchDTO.getSearchString());
+
+		return commentImpl.getAllCommentsForMovie(movieid);
+
+	}
+
+	/***********************************************************************************************/
+
+	/* Rating APIs */
+
+	/***********************************************************************************************/
+
 	@ResponseStatus(HttpStatus.CREATED)
 	@RequestMapping(value = "/addrating", method = RequestMethod.POST)
 	@ResponseBody
 	public void addMovie(@Valid @RequestBody UserRatingsDTO userRatingDTO, @CookieValue("userid") int userid) {
-    
+
 		userRatingImpl.addUserRating(userRatingDTO, userid);
-		
+
 	}
-	
-	
-/***********************************************************************************************/   
 
-									/* Analytics APIs */
 
-/***********************************************************************************************/
-    
-    
+	/***********************************************************************************************/   
+
+	/* Analytics APIs */
+
+	/***********************************************************************************************/
+
+
 	@ResponseStatus(HttpStatus.OK)
 	@RequestMapping(value = "/topratedmovies", method = RequestMethod.POST)
 	@ResponseBody
 	public List<Movie> getMoviesByTopRating() {
-    
+
 		List<Movie> movies = analyticsImpl.getTopRatedSixMovies();
-		
+
 		return movies; 
-		
+
 	}
-    
-    
+
+
 	@ResponseStatus(HttpStatus.OK)
 	@RequestMapping(value = "/recentlywatchedmovies", method = RequestMethod.POST)
 	@ResponseBody
 	public List<Movie> getRecentlyWatchedMovies(@CookieValue("userid") int userid) {
-    
+
 		List<Movie> movies = analyticsImpl.getRecentlyWatchedMovies(userid);
-		
+
 		return movies; 
-		
+
 	}
-    
-/***********************************************************************************************/
-    
-    
-    
-    
-    
-    
-    
-    
-    
-    
-    
-    
-    
-    
-    
-    
-    
-    
-    
-    
-    
-    
-    
-    
-    
-    
-    
-    
-    
+
+	/***********************************************************************************************/
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
 	// User Related Operations
 	@RequestMapping(value = "/demo/{username}", method = RequestMethod.GET)
 	@ResponseBody
@@ -404,24 +414,24 @@ public class MovieAppController extends WebMvcConfigurerAdapter {
 		demoDto.setMessage(demoInterface.printWelcomeMessage(username));
 		return demoDto;
 	}
-	
+
 	@RequestMapping(value = "/demo/test/{id}", method = RequestMethod.GET)
 	@ResponseBody
 	public Test dbtest(@PathVariable int id) {
-		
+
 		Test test = new Test();
-		
+
 		DateTime dt = new DateTime();
-				
+
 		test.setTestIdString(dt.toString());
 		test.setTestString("Last loaded record => " + id);
 		testDao.saveTest(test);
 		Test savedTest = testDao.getTest(test);
-		
+
 		return savedTest;
-	
+
 	}
-	
-	
+
+
 
 }
